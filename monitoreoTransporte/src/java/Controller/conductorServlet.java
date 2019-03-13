314b4b056a39a51c1d20f216564e5d7085f83e14
MarioSerna <mario.serna.morales02@gmail.com>
2019-03-13 13:53:44 -0700

    Servlets2

--- monitoreoTransporte/src/java/Controller/conductorServlet.java
+++ monitoreoTransporte/src/java/Controller/conductorServlet.java
@@ -6,7 +6,6 @@
 package Controller;
 
 import database.baseDatos;
-import entity.PerfilUsuario;
 import entity.Usuarios;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -20,8 +19,8 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author mario
  */
-@WebServlet(name = "tipoUsuarioServlet", urlPatterns = {"/tipoUsuarioServlet"})
-public class tipoUsuarioServlet extends HttpServlet {
+@WebServlet(name = "conductorServlet", urlPatterns = {"/conductorServlet"})
+public class conductorServlet extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -34,60 +33,67 @@ public class tipoUsuarioServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-         baseDatos _baseDatos = new baseDatos();
-        perfilUsuarioDAOImpl _perfilUsuarioDao = new perfilUsuarioDAOImpl(_baseDatos.getConnection());
+        
+            baseDatos _baseDatos = new baseDatos();
+        conductorDAOImpl _usuarioDao = new conductorDAOImpl(_baseDatos.getConnection());
         
         int _opcion = (request.getParameter("op")!= null && !request.getParameter("op").isEmpty()? Integer.valueOf(request.getParameter("op")):0);
+        int _id = (request.getParameter("id") != null && !request.getParameter("id").isEmpty() ? Integer.valueOf(request.getParameter("id")): 0);
         String _nombreCompleto = (request.getParameter("nombre") != null && !request.getParameter("nombre").isEmpty() ? request.getParameter("nombre"): new String());
-        String _descripcion = (request.getParameter("descripcion") != null && !request.getParameter("descripcion").isEmpty() ? request.getParameter("descripcion"): new String());
+        int _edad = (request.getParameter("edad") != null && !request.getParameter("edad").isEmpty() ? Integer.valueOf(request.getParameter("edad")): 0);
+        String _correo = (request.getParameter("correo") != null && !request.getParameter("correo").isEmpty() ? request.getParameter("correo"): new String());
+        String _contrasena = (request.getParameter("contrasena") != null && !request.getParameter("contrasena").isEmpty() ? request.getParameter("contrasena"): new String());
+        String _puntoCaptura = (request.getParameter("telefono") != null && !request.getParameter("telefono").isEmpty() ? request.getParameter("telefono"): new String());
         
         try {
             switch(_opcion){
                 case 1 :{
                     //Agregar
-                    PerfilUsuario _perfilUsuario = new PerfilUsuario();
-                    _perfilUsuario.setNombreCompleto(_nombreCompleto.toUpperCase());
-                    _perfilUsuario.setDescripcion(_descripcion);
+                    Usuarios _usuario = new Usuarios();
+                    _usuario.setNombreCompleto(_nombreCompleto.toUpperCase());
+                    _usuario.setCuentaCorreo(_correo);
+                    _usuario.setContrasena(_contrasena);
                     
-                    _perfilUsuarioDao.agregarRegistro(_perfilUsuario);
+                    _usuarioDao.agregarRegistro(_usuario);
                     break;
                 }
                 case 2 :{
                     //Editar
-                   PerfilUsuario _perfilUsuario = new PerfilUsuario();
-                    _perfilUsuario.setNombreCompleto(_nombreCompleto.toUpperCase());
-                    _perfilUsuario.setDescripcion(_descripcion);
+                    Usuarios _usuario = new Usuarios();
+                    _usuario.setNombreCompleto(_nombreCompleto.toUpperCase());
+                    _usuario.setCuentaCorreo(_correo);
+                    _usuario.setContrasena(_contrasena);
                     
-                    _perfilUsuarioDao.modificarRegistro(_perfilUsuario);
+                    _usuarioDao.modificarRegistro(_usuario);
                     break;
                 }
                 case 3: {
-                   PerfilUsuario _perfilUsuario = new PerfilUsuario();
-                    _perfilUsuarioDao.eliminarRegistro(_perfilUsuario);
+                   
                     break;
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+        
+    }
         /*
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-        
             /* TODO output your page here. You may use following sample code. */
-        /*
-            out.println("<!DOCTYPE html>");
+        /*  
+        out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet tipoUsuarioServlet</title>");            
+            out.println("<title>Servlet conductorServlet</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet tipoUsuarioServlet at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Servlet conductorServlet at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
     }
-/*
+*/
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -127,5 +133,4 @@ public class tipoUsuarioServlet extends HttpServlet {
         return "Short description";
     }// </editor-fold>
 
-}
 }
